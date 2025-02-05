93be94a01ad664501a9b39643eaf879eeba4c242
Maelmin Henge <contact@hengebytes.com>
2025-02-05 23:55:08 +0200

    fix deprication

--- src/DependencyInjection/HBWebserviceCoreAsyncExtension.php
+++ src/DependencyInjection/HBWebserviceCoreAsyncExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
 class HBWebserviceCoreAsyncExtension extends Extension
 {
